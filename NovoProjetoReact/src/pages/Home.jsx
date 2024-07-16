2b5f9d9e38e7dfdd34579d252f85236d15f900c0
JzBreno <jose.breno.sousa61@aluno.ifce.edu.br>
2024-07-16 06:39:25 -0300

    Teste de page Home

--- NovoProjetoReact/src/pages/Home.jsx
+++ NovoProjetoReact/src/pages/Home.jsx
@@ -3,6 +3,12 @@ const Home = () => {
     return (
         <>
         <h1>Home</h1>
+        <div>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Exercitationem, voluptatibus praesentium hic sapiente adipisci, ipsam inventore asperiores ea esse, autem ullam incidunt. Vitae illum cupiditate quo tenetur corrupti aut eaque.</p>
+        </div>
+        <label htmlFor="Teste"></label>
+        <input type="text" />
+        <button>Teste</button>
         </>
       );
 }
